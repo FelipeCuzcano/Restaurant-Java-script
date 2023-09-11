5da3e7fbeb77b14b5cc9b1598f3eea9bf5ed1f34
FelipeCuzcano <131031969+FelipeCuzcano@users.noreply.github.com>
2023-09-11 13:00:21 -0500

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,20 @@
+alert("Bienvenido a la pagina de JavaScript, para poder mostrar los resultados presiona la tecla F12")
+var num1 = parseInt(prompt("Ingrese el primer numero"));
+var num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+
+
+
+//Procesar Datos
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multi = num1 * num2;
+var div = num1 / num2;
+var residuo = num1 % num2
+
+//Mostrar Datos
+console.log("La suma es: " + suma);
+console.log("La resta es: " + resta)
+console.log("El producto de los numeros es: " + multi);
+console.log("El cociente de los numeros es: " + div);
+console.log("El residuo de los numeros es: " + residuo);
